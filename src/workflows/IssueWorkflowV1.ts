375ced22b847af23dbb8be1f07acea83c2ab769a
Gianluca Durelli <durellinux@gmail.com>
2026-05-31 11:26:04 +0200

    chore(workflows): fix restate execution

--- src/workflows/IssueWorkflowV1.ts
+++ src/workflows/IssueWorkflowV1.ts
@@ -1,9 +1,6 @@
 import * as restate from "@restatedev/restate-sdk";
-import {IssueState} from '../../issue-state';
-import {executeGemini} from "./gemini";
-import {fetchIssueState, updateRepository} from "./actions/issuesActions";
+import {fetchIssueDetails, fetchIssueState, updateRepository} from "./actions/issuesActions";
 import {setupWorkspace, findPullRequest} from "./actions/workspaceActions";
-import {buildPlanningPrompt, commitPlan, deletePlanningSession, thumbsUpPlan} from "./actions/planActions";
 import {planningLoop} from "./blocks/planningLoop";
 import {implementationBlock} from "./blocks/implementationBlock";
 import {prReviewLoop} from "./blocks/prReviewLoop";
@@ -16,8 +13,9 @@ export const IssueWorkflowV1 = restate.workflow({
             const workflowUrl = `http://localhost:8080/visualize/IssueWorkflowV1/${ctx.key}`;
 
             const { state } = await ctx.run('fetch-initial-state', () => fetchIssueState(issueNumber, issueRepo));
+            const ghIssue = await ctx.run('fetch-github-details', () => fetchIssueDetails(issueNumber, issueRepo));
             await ctx.run('create-issue', () => updateRepository(issueNumber, params.title, params.url, state, workflowUrl));
-            await ctx.run('setup-workspace', () => setupWorkspace(issueNumber, issueRepo));
+            await ctx.run('setup-workspace', () => setupWorkspace(issueNumber, issueRepo, ghIssue.branch));
 
             await planningLoop(ctx, issueNumber, issueRepo, params.title, params.url, workflowUrl);
             await implementationBlock(ctx, issueNumber, issueRepo, params.title, params.url, workflowUrl);
